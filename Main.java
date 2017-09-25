0da74eacbfc07f466e6ec6fe67ce95332a320f20
106021055 <32056108+106021055@users.noreply.github.com>
2017-09-25 16:33:44 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import jdk.nashorn.internal.runtime.regexp.joni.ScanEnvironment;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float val = 2 ;
+        float v1 = 900 ;
+        System.out.println(Math.round(val*0.26418f*10f)/10f);
+        System.out.println(Math.round(v1/600*10f)/10f);
+
+    }
+}
